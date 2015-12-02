8cd5eba15082201f72654863eb68baa4edd32f77
Michael Treser <Michael.Treser@Michael_T-PC.Giant-Steps.local>
2015-12-02 18:29:02 +0200

    Adding resolve method for Users config.

--- builds/dev/app/users/users.config.js
+++ builds/dev/app/users/users.config.js
@@ -18,7 +18,25 @@
 					controller: 'UsersCtrl',
 					controllerAs: 'uc',
 					templateUrl: 'app/users/users.html',
-					authenticate: true
+					authenticate: false,
+					resolve: {
+						auth: /* @ngInject */ function(dbc, $q, $state) {
+							var deferred = $q.defer();
+							setTimeout(function(){
+								console.log('Resolve auth.', dbc.get$Auth().$getAuth());
+								if (dbc.get$Auth().$getAuth() !== null) {
+									console.log('Resolve!');
+									deferred.resolve();
+								} else {
+									console.log('Reject!');
+									$state.go('signin');
+									deferred.reject();
+								}
+							}, 50);
+
+							return deferred.promise;
+						}
+					}
 				})
 		}
 
