9ab1ea2dda476b78d6e9f2f01193cbcf3b732d90
Michael Treser <armikael2@gmail.com>
2015-12-17 16:40:49 +0300

    Trying to create progress directive.

--- builds/dev/app/progress/progress.directive.js
+++ builds/dev/app/progress/progress.directive.js
@@ -9,17 +9,30 @@
     function kpstrProgress() {
         var directive = {
             restrict: 'EAC',
+            scope: {
+                max: '='
+            },
             link: link,
-            //replace: true,
+            replace: true,
             templateUrl: 'app/progress/progress.html'
         };
 
         return directive;
 
         function link(scope, elem, attrs) {
+            console.log("DIRECTIVE", scope, elem, attrs)
+            var parent = elem.parent();
+            console.log('parent', elem.parent());
+            console.log('HA', parent.find('p'));
+            console.log('val', parent.find('p').html());
+            //scope.exercisesCount = scope.max.split(',').length;
+            //scope.exercisesCount = 20;
+            //console.log('exs length', scope.exercisesCount.length);
+            //console.log('MAX: ', scope.exercisesCount);
+
             console.log('exerciseProgress directive link', attrs);
 
-            console.log('parent', elem.parent());
+
         }
     }
 
