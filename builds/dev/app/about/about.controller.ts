3b26902fd2ce842c6c55866f7b1fbc6e659bf6ce
Michael Treser <Michael.Treser@Michael_T-PC.Giant-Steps.local>
2016-01-12 10:26:50 +0200

    Playing with TypeScript.

---
+++ builds/dev/app/about/about.controller.ts
@@ -0,0 +1,25 @@
+module app.about.controller {
+
+    interface aboutControllerModel {
+        title: string,
+        employees: [any],
+        writeFeedback(): void
+    }
+
+    
+    class AboutController implements aboutControllerModel {
+        title: 'About us';
+        employees: ['Ben', 'Roman', 'Moshe'];
+        writeFeedback(): void {
+            console.log('feedback');
+        }
+
+        constructor() {
+
+        }
+
+    }
+
+    angular.module('kpStr.about')
+        .controller('AboutCtrl', AboutController);
+}
